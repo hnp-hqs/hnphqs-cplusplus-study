7804174c767eabcba5528e0aa5e739d94fbaab56
hnp-hqs <wangsyyyds@foxmail.com>
2025-07-09 23:00:44 +0800

    改个文件夹

--- 语法/2.7.1.cpp
+++ 语法/2.7.1.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main(){
-	char my_name[]="王诗雨";
-	char my_address[]="重庆市开州区";
+	char my_name[]="雨儿";
+	char my_address[]="重庆";
 	cout<<"我的名字:"<<my_name<<endl
 		<<"我的地址:"<<my_address<<endl;
 	return 0;
